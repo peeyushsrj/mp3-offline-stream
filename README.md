51f17fadbd716dd220f2a9873dd8032e2abc50e0
Peeyush Singh <peeyushsrj@gmail.com>
2019-09-15 15:07:05 +0530

    v: 0.3

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 mp3-offline-stream
-=================
+==================
 
 cloud with a substring match on song titles.
 
@@ -14,13 +14,13 @@ Use case:
 ## Use
 
 1. Clone repo.
-2. Move index.rb and views/ directory to the directory where music files are.
+2. Move `index.rb` and `views/` directory to the directory where music files are.
 3. Search a song: `0.0.0.0:4567/?q=writer`
 
 
 ## Roadmap
 
-- To be written ( required things as 0.2)
+- To be written ( required things as 1.0)
 
 ### Changelog
 
@@ -30,3 +30,10 @@ v: 0.2
 - Ruby as an executable (for linux)
 - String match better downcased
 - Priting sample sizes before server starts
+
+v: 0.3
+======
+
+- changed: port default 4567 => 4444 
+- added: form for input
+- added: mobile zoom supported
--- index.rb
+++ index.rb
@@ -1,6 +1,8 @@
 #!/usr/bin/env ruby
 require 'sinatra'
 set :public_folder, '.'
+set :bind, '0.0.0.0'
+set :port, 4444
 
 mp3Files = Dir.glob("**/*.mp3")
 puts "Scanned #{mp3Files.length} mp3 files"
@@ -14,17 +16,16 @@ end
 get '/' do
   inputFile = params['q']
   if !inputFile
-    return "mp3 station"
-  else
-    inputFile = inputFile.downcase
-    @songs = Array.new
-    for mp3Path, mp3File in mp3DB
-      if mp3File.include?inputFile
-        @songs.push({"name": mp3File ,"path": mp3Path})
-      end
-    end
-    erb :search
+  	inputFile = " "
   end
+  
+  inputFile = inputFile.downcase
+  @songs = Array.new
+  for mp3Path, mp3File in mp3DB
+    if mp3File.include?inputFile
+      @songs.push({"name": mp3File ,"path": mp3Path})
+    end
+  end
+  erb :search
+  
 end
-
-
